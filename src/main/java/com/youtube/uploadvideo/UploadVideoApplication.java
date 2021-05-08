34520c65326757ac6878c6a6a1ba18d332799fb7
Oleksandr Kravchuk <oakravchuk@luxoft.com>
2021-05-08 21:33:30 +0300

    added uploading files functionality

--- src/main/java/com/youtube/uploadvideo/UploadVideoApplication.java
+++ src/main/java/com/youtube/uploadvideo/UploadVideoApplication.java
@@ -1,13 +1,28 @@
 package com.youtube.uploadvideo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.youtube.uploadvideo.storage.StorageProperties;
+import com.youtube.uploadvideo.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class UploadVideoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UploadVideoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
+
