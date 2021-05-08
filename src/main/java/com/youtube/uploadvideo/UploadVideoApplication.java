a4955abdd869a56f7a9b63f040b123c50a6e0a9e
Oleksandr Kravchuk <oakravchuk@luxoft.com>
2021-05-08 21:08:20 +0300

    init: spring initializr

---
+++ src/main/java/com/youtube/uploadvideo/UploadVideoApplication.java
@@ -0,0 +1,13 @@
+package com.youtube.uploadvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadVideoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youtube/uploadvideo/UploadVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtube.uploadvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
